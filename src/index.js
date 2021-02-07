91fc84c0977f8efac95b7fba0486c343b86b8f56
jkorzeniewski <jakubkorzeniewski380@gmail.com>
2021-02-07 01:52:47 +0100

    Added basic PWA functionality

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import TodoList from "./components/TodoList/TodoList";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 const destination = document.querySelector("#container");
 
@@ -11,3 +13,5 @@ ReactDOM.render(
     </div>,
     destination
 );
+
+serviceWorkerRegistration.register();
